53b004a645bd07f6301e4c69d027973bda0b7aeb
Husan <shomurodvhusan130@gmail.com>
2024-07-23 19:14:33 +0500

    fix

--- src/components/videoTwo/VideoTwo.vue
+++ src/components/videoTwo/VideoTwo.vue
@@ -7,7 +7,7 @@ const videoDescription = "Bu videoda, kimyo xonasidagi asboblar va alanganing tu
     <div class="container">
         <div class="video">
             <div class="video-item">
-                <video class="video-mp4" src="@/assets/video/2.mp4" controls></video>
+                <!-- <video class="video-mp4" src="@/assets/video/2.mp4" controls></video> -->
             </div>
             <div class="video-item">
                 <h3 class="video-item-title">{{ videoTitle }}</h3>
