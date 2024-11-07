447b7332cd554087369f76b02fcf95c88136ee77
Husan <shomurodovhusan130@gmail.com>
2024-11-08 01:20:43 +0500

    change some UI

--- src/views/InfoView/InfoView.vue
+++ src/views/InfoView/InfoView.vue
@@ -6,13 +6,20 @@
             <div class="info-item" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
                 <img class="info-img1" src="@/assets/img/ustoz.jpg" alt="uatoz" />
             </div>
+            <p class="info__info">
+                A.Avloniy nomidagi pedagogik mahorat milliy instituti Mustaqil izlanuvchisi Jumanova Soxiba Yuldoshovna
+                ÒQUVCHILARDA UMUMÒQUV KÒNIKMALARNI SHAKLLANTIRISH METODIKASI (kimyo fani misolida )
+                13.00.02- Ta'lim va tarbiya nazaryasi metodikasi (kimyo)
+                Pedagogika fanlari bòyicha falsafa doktori (PhD ) ilmiy darajasini olish uchun yozgan dissertatsiyasi
+            </p>
             <div class="info-item" data-aos="fade-left" data-aos-offset="300" data-aos-easing="ease-in-sine">
                 <img class="info-img-two" src="@/assets/img/guvohnoma.png" alt="guvohnoma" />
             </div>
         </div>
         <div class="info-box" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
             <h2 class="info-title">Foydalanilgan adabiyotlar</h2>
-            <p class="info-text">Kimyo 7-sinf uchun darslik. I Asqarov va boshqalar tomonidan ishlab chiqilgan <span>Toshkent Respublika ta'lim markazi, 2022</span></p>
+            <p class="info-text">Kimyo 7-sinf uchun darslik. I Asqarov va boshqalar tomonidan ishlab chiqilgan
+                <span>Toshkent Respublika ta'lim markazi, 2022</span></p>
         </div>
     </div>
 </template>
